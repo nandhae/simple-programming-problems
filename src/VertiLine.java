d32d80fd4d8e9fcf749cfb793f244ea7811a5529
nandhagr8 <nandhagr8@gmail.com>
2017-12-01 19:27:57 +0530

    VertiLine

---
+++ src/VertiLine.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class VertiLine {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int no = scanner.nextInt();
+        printAsterisks(no);
+
+    }
+
+    private static void printAsterisks(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+
+    }
+}
