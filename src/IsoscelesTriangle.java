1c4a6e5b4aa29f7c2d1f3ff5e758bf515deabc2c
nandhagr8 <nandhagr8@gmail.com>
2017-12-01 21:02:57 +0530

    IsoscelesTriangle

--- src/IsoscelesTriangle.java
+++ src/IsoscelesTriangle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class IsoscelesTriangleDiamond {
+public class IsoscelesTriangle {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int no=scanner.nextInt();
@@ -10,31 +10,21 @@ public class IsoscelesTriangleDiamond {
 
     private static void printAsterisks(int n){
 
+
         int space=n-1;
         for (int i =1 ; i <= n; i++) {
-            for (int j = 1; j <=space; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= 2 * i -1; k++) {
-                System.out.print("*");
-            }
-            space--;
-            System.out.println();
-        }
-        space=1;
-        for (int i =1 ; i <= n-1; i++) {
             for (int j = 1; j <= space; j++) {
                 System.out.print(" ");
             }
-            for (int k =1 ; k <= 2 * (n-i) -1; k++) {
+            for (int k = 1; k <= 2 *i -1; k++) {
                 System.out.print("*");
             }
-            space++;
+
+            space--;
             System.out.println();
         }
+
 
 
     }
 }
-
-
